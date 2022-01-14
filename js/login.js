647de14c9e1fa6c49a8826238802ce9a5f785f0a
vuxuanson0810 <you@example.com>
2022-01-14 13:03:11 +0700

    js login

---
+++ js/login.js
@@ -0,0 +1,9 @@
+function btn(){
+    var arr = document.getElementsByTagName('input');
+    var email = arr[0].value;
+    var password = arr[1].value;
+    
+    if(email == ""|| password == "" ||){
+        alert("dang nhap lai");
+    }
+}
